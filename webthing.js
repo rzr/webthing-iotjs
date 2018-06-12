f1b086f42a5cd43701fdb8b36297c2bdea1a6802
Philippe Coval <p.coval@samsung.com>
2018-06-12 23:48:48 +0200

    lib: remove actions, events
    May not be reintroduced later as not critical
    
    Change-Id: I9d8b52c1d1ceca84a7ba36aa8c77cbc8e0fd2a0d
    Origin: https://github.com/tizenteam/webthing-node
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- webthing.js
+++ webthing.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports = {
-  Action: require('./lib/action'),
-  Event: require('./lib/event'),
   Property: require('./lib/property'),
   Thing: require('./lib/thing'),
   Value: require('./lib/value'),
