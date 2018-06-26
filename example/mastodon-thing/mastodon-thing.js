7007f5637c7791373ad08926685f2218b9e8d7ed
Philippe Coval <p.coval@samsung.com>
2018-06-26 13:32:36 +0200

    example: mastodon: Add MultiLevel thing that blog
    Usage:
    
        NODE_PATH=../.. node mastodon-thing.js
    
    Change-Id: I338435ea6c2f287f88de9fac7e52f98ed3593cac
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ example/mastodon-thing/mastodon-thing.js
@@ -0,0 +1,76 @@
+// -*- mode: js; js-indent-level:2;  -*-
+// SPDX-License-Identifier: MPL-2.0
+/**
+ *
+ * Copyright 2018-present Samsung Electronics France SAS, and other contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+const {
+  Property,
+  SingleThing,
+  Thing,
+  Value,
+  WebThingServer,
+} = require('webthing');
+
+const fs = require('fs');
+const Mastodon = require('mastodon-lite');
+const conf = '.mastodon-lite.json';
+const config = JSON.parse(fs.readFileSync(conf, 'utf8'));
+const mastodon = Mastodon(config);
+let lastHandle = 0;
+
+function handleLevelUpdate(value) {
+  let message = value;
+  message = `https://s-opensource.org/tag/wot/#\
+ #MultiLevelSwitch is "${value}"\
+ (#MastodonLite #WebThing Actuator)\
+ ~ @TizenHelper`;
+  console.log(message);
+  const now = new Date();
+  if ((now - lastHandle) > 60 * 1000) {
+    mastodon.post(message);
+    lastHandle = now;
+  }
+}
+
+function makeThing() {
+  const thing = new Thing('MastodonMultiLevelSwitchExample',
+                          'multiLevelSwitch',
+                          'An actuator example that just blog');
+
+  thing.addProperty(
+    new Property(thing,
+                 'level',
+                 new Value(0, handleLevelUpdate),
+                 {
+                   label: 'Level',
+                   type: 'number',
+                   description: 'Whether the output is changed',
+                 }));
+  return thing;
+}
+
+function runServer() {
+  const port = process.argv[2] ? Number(process.argv[2]) : 8888;
+  const url = `http://localhost:${port}/properties/level`;
+  console.log(`Usage:\n
+ ${process.argv[0]} ${process.argv[1]} [port]\n
+Try:\n
+curl -X PUT -H 'Content-Type: application/json' \
+--data '{"level": 42}' "${url}"\n
+`);
+
+  const thing = makeThing();
+  const server = new WebThingServer(new SingleThing(thing), port);
+  process.on('SIGINT', () => {
+    server.stop();
+    process.exit();
+  });
+  server.start();
+}
+
+runServer();
