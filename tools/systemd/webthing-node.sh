c825a107da568e6905553fa18b6a1266855ff388
Philippe Coval <p.coval@samsung.com>
2018-07-10 10:27:35 +0200

    platform: flex: Run service on boot
    Change-Id: If787f31eb4ff3af0ee8a836c0138332c1f1a1741
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

---
+++ tools/systemd/webthing-node.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+which npm
+pwd
+ls node_modules || npm install
+npm start
