62742e8aab553b4450e2e806ce174edf96f79e21
Philippe Coval <p.coval@samsung.com>
2018-10-06 16:19:57 +0200

    build: Also port travis.sh but unused yet
    Just to avoid unexpected failure
    
    Change-Id: I38039134835d06fe4bf42dc5f139ecbcde517bdd
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- travis.sh
+++ travis.sh
@@ -1,23 +1,7 @@
 #!/bin/bash -e
+set -x
 
-# run eslint first
-npm run lint
-
-# clone the webthing-tester
-git clone https://github.com/mozilla-iot/webthing-tester
-pip3 install --user -r webthing-tester/requirements.txt
-
-export NODE_PATH=.
-# build and test the single-thing example
-node example/single-thing.js &
-EXAMPLE_PID=$!
-sleep 5
-./webthing-tester/test-client.py --debug
-kill -15 $EXAMPLE_PID
-
-# build and test the multiple-things example
-node example/multiple-things.js &
-EXAMPLE_PID=$!
-sleep 5
-./webthing-tester/test-client.py --path-prefix "/0"
-kill -15 $EXAMPLE_PID
+make lint
+make build
+make check
+make test
