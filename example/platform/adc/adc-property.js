1f58281eca524d216967f2bfcbea2158bce5ef6e
Philippe Coval <p.coval@samsung.com>
2018-10-17 19:49:50 +0200

    iotjs: babel: Transpiled (babel/commit/iotjs)
    Change-Id: I33678bb4f98a98429f599a0d4015e07a14759024

--- example/platform/adc/adc-property.js
+++ example/platform/adc/adc-property.js
@@ -9,56 +9,58 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
  */
-
-const console = require('console');
-
-// Disable logs here by editing to '!console.log'
-const log = console.log || function() {};
-const verbose = !console.log || function() {};
+var console = require('console'); // Disable logs here by editing to '!console.log'
 
 
-let webthing;
+var log = console.log || function () {};
+
+var verbose = !console.log || function () {};
+
+var webthing;
+
 try {
   webthing = require('../../../webthing');
 } catch (err) {
   webthing = require('webthing');
 }
-const Property = webthing.Property;
-const Value = webthing.Value;
 
-const adc = require('adc');
+var Property = webthing.Property;
+var Value = webthing.Value;
+
+var adc = require('adc');
 
 function AdcInProperty(thing, name, value, metadata, config) {
-  const self = this;
-  const valueObject = new Value(Number(value));
+  var _this = this;
+
+  var self = this;
+  var valueObject = new Value(Number(value));
   Property.call(this, thing, name, valueObject, {
     '@type': 'LevelProperty',
-    label: (metadata && metadata.label) || `Level: ${name}`,
+    label: metadata && metadata.label || "Level: ".concat(name),
     type: 'number',
     readOnly: true,
-    description:
-    (metadata && metadata.description) ||
-      (`ADC Sensor on pin=${config.pin}`),
+    description: metadata && metadata.description || "ADC Sensor on pin=".concat(config.pin)
   });
   {
     config.frequency = config.frequency || 1;
     config.range = config.range || 0xFFF;
     this.period = 1000.0 / config.frequency;
     this.config = config;
-    this.port = adc.open(config, (err) => {
-      log(`log: ADC: ${self.getName()}: open: ${err} (null expected)`);
+    this.port = adc.open(config, function (err) {
+      log("log: ADC: ".concat(self.getName(), ": open: ").concat(err, " (null expected)"));
+
       if (err) {
-        console.error(`error: ADC: ${self.getName()}: Fail to open:\
- ${config.pin}`);
+        console.error("error: ADC: ".concat(self.getName(), ": Fail to open: ").concat(config.pin));
         return null;
       }
-      self.inverval = setInterval(() => {
-        let value = Number(self.port.readSync());
-        verbose(`log: ADC:\
- ${self.getName()}: update: 0x${value.toString(0xF)}`);
+
+      self.inverval = setInterval(function () {
+        var value = Number(self.port.readSync());
+        verbose("log: ADC: ".concat(self.getName(), ": update: 0x").concat(value.toString(0xF)));
         value = Number(Math.floor(100.0 * value / self.config.range));
+
         if (value !== self.lastValue) {
-          log(`log: ADC: ${self.getName()}: change: ${value}%`);
+          log("log: ADC: ".concat(self.getName(), ": change: ").concat(value, "%"));
           self.value.notifyOfExternalUpdate(value);
           self.lastValue = value;
         }
@@ -66,15 +68,16 @@ function AdcInProperty(thing, name, value, metadata, config) {
     });
   }
 
-  self.close = () => {
+  self.close = function () {
     try {
-      this.inverval && clearInterval(this.inverval);
-      this.port && this.port.closeSync();
+      _this.inverval && clearInterval(_this.inverval);
+      _this.port && _this.port.closeSync();
     } catch (err) {
-      console.error(`error: ADC: ${this.getName()} close:${err}`);
+      console.error("error: ADC: ".concat(_this.getName(), " close:").concat(err));
       return err;
     }
-    log(`log: ADC: ${this.getName()}: close:`);
+
+    log("log: ADC: ".concat(_this.getName(), ": close:"));
   };
 
   return this;
@@ -84,6 +87,7 @@ function AdcProperty(thing, name, value, metadata, config) {
   if (config.direction === 'in') {
     return new AdcInProperty(thing, name, value, metadata, config);
   }
+
   throw 'error: Invalid param';
 }
 
--- example/platform/adc/index.js
+++ example/platform/adc/index.js
@@ -1,28 +1,27 @@
 // -*- mode: js; js-indent-level:2;  -*-
 // SPDX-License-Identifier: ISC
+
 /**
  * Copyright 2018-present Samsung Electronics France SAS, and other contributors
  *
  * This Source Code Form is subject to the terms of the ICS Licence:
  * https://spdx.org/licenses/ISC.html#licenseText
  */
-
-const fs = require('fs');
+var fs = require('fs');
 
 function Adc() {
-  this.open = function(config, callback) {
+  this.open = function (config, callback) {
     this.config = config;
     fs.access(config.device, fs.R_OK, callback);
     return this;
   };
 
-  this.readSync = function() {
-    const contents = fs.readFileSync(this.config.device, 'ascii');
+  this.readSync = function () {
+    var contents = fs.readFileSync(this.config.device, 'ascii');
     return contents;
   };
 
-  this.closeSync = function() {
-  };
+  this.closeSync = function () {};
 }
 
 module.exports = new Adc();
