24c467a4354f6f2feaee0f81672303b8e40b74e3
Philippe Coval <p.coval@samsung.com>
2018-10-23 17:19:45 +0200

    systemd: Use make to prepare env instead of npm
    Change-Id: I2f3b53090c156cc47068f9b2f63a486782735467
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- tools/systemd/webthing-node.sh
+++ tools/systemd/webthing-node.sh
@@ -3,7 +3,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-which npm
-pwd
-ls node_modules || npm install
-npm start
+make -C example/platform flex-phat/node
