2e6ec1f507e9e279676cf6d2716d5e02c2fcc94a
Philippe Coval <p.coval@samsung.com>
2018-11-14 23:24:14 +0100

    iotjs: babel: Transpiled (babel/commit/iotjs)
    Change-Id: I1b8d3ed2b0a8608289820172517a7b2fab8ab5a3
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- webthing.js
+++ webthing.js
@@ -4,9 +4,8 @@ module.exports = {
   Property: require('./lib/property'),
   Thing: require('./lib/thing'),
   Value: require('./lib/value'),
-  server: require('./lib/server'),
+  server: require('./lib/server')
 };
-
 module.exports.MultipleThings = module.exports.server.MultipleThings;
 module.exports.SingleThing = module.exports.server.SingleThing;
 module.exports.WebThingServer = module.exports.server.WebThingServer;
