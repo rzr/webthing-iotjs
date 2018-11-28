9eb9a27a4184237137c5465ebd0862a77ea827b0
Philippe Coval <p.coval@samsung.com>
2018-11-28 18:43:25 +0100

    iotjs: babel: Transpiled (babel/commit/iotjs)
    Change-Id: Iceb123e1c8eb9cf8a2fdee9d60c0e47bb7f04cb4
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- example/platform/board/artik530.js
+++ example/platform/board/artik530.js
@@ -9,61 +9,67 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
  */
+var webthing;
 
-let webthing;
 try {
   webthing = require('../../../webthing');
 } catch (err) {
   webthing = require('webthing');
 }
-const Thing = webthing.Thing;
 
-const AdcProperty = require('../adc/adc-property');
-const GpioProperty = require('../gpio/gpio-property');
+var Thing = webthing.Thing;
+
+var AdcProperty = require('../adc/adc-property');
+
+var GpioProperty = require('../gpio/gpio-property');
 
 function ARTIK530Thing(name, type, description) {
-  const self = this;
-  Thing.call(this,
-             name || 'ARTIK530',
-             type || [],
-             description || 'A web connected ARTIK530 or ARTIK720');
+  var _this = this;
+
+  var self = this;
+  Thing.call(this, name || 'ARTIK530', type || [], description || 'A web connected ARTIK530 or ARTIK720');
   {
-    this.pinProperties = [
-      new GpioProperty(this, 'RedLED', false,
-                       {description:
-                             'Red LED on interposer board (on GPIO28)'},
-                       {direction: 'out', pin: 28}),
-      new GpioProperty(this, 'BlueLED', false,
-                       {description:
-                             'Blue LED on interposer board (on GPIO38)'},
-                       {direction: 'out', pin: 38}),
-      new GpioProperty(this, 'Up', true,
-                       {description:
-                             'SW403 Button: Nearest board edge,\
- next to red LED (on GPIO30)'},
-                       {direction: 'in', pin: 30}),
-      new GpioProperty(this, 'Down', true,
-                       {description:
-                             'SW404 Button: Next to blue LED (on GPIO32)'},
-                       {direction: 'in', pin: 32}),
-      new AdcProperty(this, 'ADC0', 0,
-                      {description: 'Analog port of ARTIK05x'},
-                      {direction: 'in',
-                       device: '/sys/bus/platform/devices\
-/c0053000.adc/iio:device0/in_voltage0_raw'}),
-      new AdcProperty(this, 'ADC1', 0,
-                      {description: 'Analog port of ARTIK05x'},
-                      {direction: 'in',
-                       device: '/sys/bus/platform/devices/\
-c0053000.adc/iio:device0/in_voltage1_raw'}),
-    ];
-    this.pinProperties.forEach((property) => {
+    this.pinProperties = [new GpioProperty(this, 'RedLED', false, {
+      description: 'Red LED on interposer board (on GPIO28)'
+    }, {
+      direction: 'out',
+      pin: 28
+    }), new GpioProperty(this, 'BlueLED', false, {
+      description: 'Blue LED on interposer board (on GPIO38)'
+    }, {
+      direction: 'out',
+      pin: 38
+    }), new GpioProperty(this, 'Up', true, {
+      description: 'SW403 Button: Nearest board edge,\
+ next to red LED (on GPIO30)'
+    }, {
+      direction: 'in',
+      pin: 30
+    }), new GpioProperty(this, 'Down', true, {
+      description: 'SW404 Button: Next to blue LED (on GPIO32)'
+    }, {
+      direction: 'in',
+      pin: 32
+    }), new AdcProperty(this, 'ADC0', 0, {
+      description: 'Analog port of ARTIK05x'
+    }, {
+      direction: 'in',
+      device: '/sys/bus/platform/devices\
+/c0053000.adc/iio:device0/in_voltage0_raw'
+    }), new AdcProperty(this, 'ADC1', 0, {
+      description: 'Analog port of ARTIK05x'
+    }, {
+      direction: 'in',
+      device: '/sys/bus/platform/devices/\
+c0053000.adc/iio:device0/in_voltage1_raw'
+    })];
+    this.pinProperties.forEach(function (property) {
       self.addProperty(property);
     });
   }
 
-  this.close = () => {
-    this.pinProperties.forEach((property) => {
+  this.close = function () {
+    _this.pinProperties.forEach(function (property) {
       property.close && property.close();
     });
   };
@@ -71,9 +77,10 @@ c0053000.adc/iio:device0/in_voltage1_raw'}),
   return this;
 }
 
-module.exports = function() {
+module.exports = function () {
   if (!module.exports.instance) {
     module.exports.instance = new ARTIK530Thing();
   }
+
   return module.exports.instance;
 };
