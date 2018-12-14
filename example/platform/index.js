d01ebcc061eb582e5db8387ec3f7a42e40e071e5
Philippe Coval <p.coval@samsung.com>
2018-12-15 00:06:22 +0100

    iotjs: babel: Transpiled (babel/commit/iotjs)
    Change-Id: Ib6ef5da6708dc8a43db655833031d516c22ecbb3
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- example/platform/index.js
+++ example/platform/index.js
@@ -9,54 +9,53 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
  */
+var console = require('console'); // TODO: disable logs here by editing to '!console.log'
 
-const console = require('console');
 
-// TODO: disable logs here by editing to '!console.log'
-const log = console.log || function() {};
+var log = console.log || function () {};
 
-let webthing;
+var webthing;
 
 try {
   webthing = require('../../webthing');
 } catch (err) {
   webthing = require('webthing');
 }
-const WebThingServer = webthing.WebThingServer;
-const SingleThing = webthing.SingleThing;
 
-// Update with different board here if needed
-let board = 'artik530';
+var WebThingServer = webthing.WebThingServer;
+var SingleThing = webthing.SingleThing; // Update with different board here if needed
+
+var board = 'artik530';
+
 if (process.iotjs && process.iotjs.board) {
   board = process.iotjs.board;
 }
+
 if (process.argv.length > 2) {
   board = String(process.argv[2]);
 }
 
-log(`log: board: ${board}: Loading`);
-const BoardThing = require(`./board/${board}`);
+log("log: board: ".concat(board, ": Loading"));
+
+var BoardThing = require("./board/".concat(board));
 
 function runServer() {
-  const port = process.argv[3] ? Number(process.argv[3]) : 8888;
-  const url = `http://localhost:${port}`;
-
-  log(`Usage:\n\
-${process.argv[0]} ${process.argv[1]} [board] [port]\n\
-Try:\ncurl -H "Accept: application/json" ${url}\
-\n`);
-  const thing = new BoardThing();
-  const server = new WebThingServer(new SingleThing(thing), port);
-  process.on('SIGINT', () => {
+  var port = process.argv[3] ? Number(process.argv[3]) : 8888;
+  var url = "http://localhost:".concat(port);
+  log("Usage:\n".concat(process.argv[0], " ").concat(process.argv[1], " [board] [port]\nTry:\ncurl -H \"Accept: application/json\" ").concat(url, "\n"));
+  var thing = new BoardThing();
+  var server = new WebThingServer(new SingleThing(thing), port);
+  process.on('SIGINT', function () {
     server.stop().then(cleanup).catch(cleanup);
-    const cleanup = () => {
+
+    var cleanup = function () {
       thing && thing.close();
-      log(`log: board: ${board}: Stopped`);
+      log("log: board: ".concat(board, ": Stopped"));
       process.exit();
     };
   });
   server.start();
-  log(`log: board: ${board}: Started`);
+  log("log: board: ".concat(board, ": Started"));
 }
 
 runServer();
