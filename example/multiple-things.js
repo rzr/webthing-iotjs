c3eb59ed9566f96f2527aa0ba03014a41d8bc173
Philippe Coval <p.coval@samsung.com>
2019-07-25 18:00:32 +0200

    iotjs: Drop promises in multiple-things.js
    Example is still working with node
    
    Change-Id: Ib03fa451bfd5c9873e8d15c23661b858f03e36a6
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- example/multiple-things.js
+++ example/multiple-things.js
@@ -110,10 +110,11 @@ function runServer() {
                                     8888);
 
   process.on('SIGINT', () => {
-    server.stop().then(() => process.exit()).catch(() => process.exit());
+    server.stop();
+    process.exit();
   });
 
-  server.start().catch(console.error);
+  server.start();
 }
 
 runServer();
