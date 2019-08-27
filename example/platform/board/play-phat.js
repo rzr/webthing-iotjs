05ad3d34eb2a0abc9ca05ec37e1029cc6ac0d46b
Philippe Coval <p.coval@samsung.com>
2019-08-27 11:01:29 +0200

    iotjs: babel: Transpiled (babel/commit/iotjs)
    Change-Id: I08b4953747f4a78a58a0972f1ee381eb3bd97cc2
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- example/platform/board/play-phat.js
+++ example/platform/board/play-phat.js
@@ -9,67 +9,72 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
  */
-
-let webthing;
+var webthing;
 
 try {
   webthing = require('../../../webthing');
 } catch (err) {
   webthing = require('webthing-iotjs');
 }
-const Thing = webthing.Thing;
 
-const GpioProperty = require('../gpio/gpio-property');
+var Thing = webthing.Thing;
+
+var GpioProperty = require('../gpio/gpio-property');
 
 function PlayPHatThing(name, type, description) {
-  const self = this;
-  Thing.call(this,
-             'urn:dev:ops:my-play-phat-1234',
-             name || 'PlayPHat',
-             type || [],
-             description || 'A web connected Play RaspberryPi Hat');
+  var _this = this;
+
+  var self = this;
+  Thing.call(this, 'urn:dev:ops:my-play-phat-1234', name || 'PlayPHat', type || [], description || 'A web connected Play RaspberryPi Hat');
   {
-    this.gpioProperties = [
-      new GpioProperty(this, 'Left', false,
-                       {description:
-                        'SW1 Sensor Button on GPIO4 (Pin7)'},
-                       {direction: 'in', pin: 4}),
-      new GpioProperty(this, 'Right', false,
-                       {description:
-                        'SW2 Sensor button on GPIO17 (Pin11)'},
-                       {direction: 'in', pin: 17}),
-      new GpioProperty(this, 'Up', false,
-                       {description:
-                        'SW3 Sensor button on GPIO22 (Pin15)'},
-                       {direction: 'in', pin: 22}),
-      new GpioProperty(this, 'Down', false,
-                       {description:
-                        'SW4 Sensor button on GPIO27 (Pin13)'},
-                       {direction: 'in', pin: 27}),
-      new GpioProperty(this, 'A', false,
-                       {description:
-                        'SW5 Sensor button on GPIO19 (Pin35)'},
-                       {direction: 'in', pin: 19}),
-      new GpioProperty(this, 'B', false,
-                       {description:
-                        'SW6 Sensor button on GPIO26 (Pin37)'},
-                       {direction: 'in', pin: 26}),
-      new GpioProperty(this, 'Start', false,
-                       {description:
-                        'SW7 Sensor button on GPIO5 (Pin29)'},
-                       {direction: 'in', pin: 5}),
-      new GpioProperty(this, 'Select', false,
-                       {description:
-                        'SW8 Sensor button on GPIO6 (Pin31)'},
-                       {direction: 'in', pin: 6}),
-    ];
-    this.gpioProperties.forEach((property) => {
+    this.gpioProperties = [new GpioProperty(this, 'Left', false, {
+      description: 'SW1 Sensor Button on GPIO4 (Pin7)'
+    }, {
+      direction: 'in',
+      pin: 4
+    }), new GpioProperty(this, 'Right', false, {
+      description: 'SW2 Sensor button on GPIO17 (Pin11)'
+    }, {
+      direction: 'in',
+      pin: 17
+    }), new GpioProperty(this, 'Up', false, {
+      description: 'SW3 Sensor button on GPIO22 (Pin15)'
+    }, {
+      direction: 'in',
+      pin: 22
+    }), new GpioProperty(this, 'Down', false, {
+      description: 'SW4 Sensor button on GPIO27 (Pin13)'
+    }, {
+      direction: 'in',
+      pin: 27
+    }), new GpioProperty(this, 'A', false, {
+      description: 'SW5 Sensor button on GPIO19 (Pin35)'
+    }, {
+      direction: 'in',
+      pin: 19
+    }), new GpioProperty(this, 'B', false, {
+      description: 'SW6 Sensor button on GPIO26 (Pin37)'
+    }, {
+      direction: 'in',
+      pin: 26
+    }), new GpioProperty(this, 'Start', false, {
+      description: 'SW7 Sensor button on GPIO5 (Pin29)'
+    }, {
+      direction: 'in',
+      pin: 5
+    }), new GpioProperty(this, 'Select', false, {
+      description: 'SW8 Sensor button on GPIO6 (Pin31)'
+    }, {
+      direction: 'in',
+      pin: 6
+    })];
+    this.gpioProperties.forEach(function (property) {
       self.addProperty(property);
     });
   }
 
-  this.close = () => {
-    this.gpioProperties.forEach((property) => {
+  this.close = function () {
+    _this.gpioProperties.forEach(function (property) {
       property.close && property.close();
     });
   };
@@ -77,9 +82,10 @@ function PlayPHatThing(name, type, description) {
   return this;
 }
 
-module.exports = function() {
+module.exports = function () {
   if (!module.exports.instance) {
     module.exports.instance = new PlayPHatThing();
   }
+
   return module.exports.instance;
 };
