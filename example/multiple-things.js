7ed34b277de242d4a9b5080719f4d21feda40241
Philippe Coval <p.coval@samsung.com>
2019-08-27 11:01:29 +0200

    iotjs: babel: Transpiled (babel/commit/iotjs)
    Change-Id: I08b4953747f4a78a58a0972f1ee381eb3bd97cc2
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- example/multiple-things.js
+++ example/multiple-things.js
@@ -1,121 +1,98 @@
 // -*- mode: js; js-indent-level:2;  -*-
 // SPDX-License-Identifier: MPL-2.0
-let webthing;
+var webthing;
+
 try {
   webthing = require('../webthing');
 } catch (err) {
   webthing = require('webthing-iotjs');
 }
-const Property = webthing.Property;
-const MultipleThings = webthing.MultipleThings;
-const Thing = webthing.Thing;
-const Value = webthing.Value;
-const WebThingServer = webthing.WebThingServer;
 
+var Property = webthing.Property;
+var MultipleThings = webthing.MultipleThings;
+var Thing = webthing.Thing;
+var Value = webthing.Value;
+var WebThingServer = webthing.WebThingServer;
 /**
  * A dimmable light that logs received commands to stdout.
  */
+
 function ExampleDimmableLight() {
   {
-    Thing.call(this,
-               'urn:dev:ops:my-lamp-1234',
-               'My Lamp',
-               ['OnOffSwitch', 'Light'],
-               'A web connected lamp'
-    );
-
-    this.addProperty(
-      new Property(
-        this,
-        'on',
-        new Value(true, (v) => console.log('On-State is now', v)),
-        {
-          '@type': 'OnOffProperty',
-          title: 'On/Off',
-          type: 'boolean',
-          description: 'Whether the lamp is turned on',
-        }));
-
-    this.addProperty(
-      new Property(
-        this,
-        'brightness',
-        new Value(50, (v) => console.log('Brightness is now', v)),
-        {
-          '@type': 'BrightnessProperty',
-          title: 'Brightness',
-          type: 'integer',
-          description: 'The level of light from 0-100',
-          minimum: 0,
-          maximum: 100,
-          unit: 'percent',
-        }));
+    Thing.call(this, 'urn:dev:ops:my-lamp-1234', 'My Lamp', ['OnOffSwitch', 'Light'], 'A web connected lamp');
+    this.addProperty(new Property(this, 'on', new Value(true, function (v) {
+      return console.log('On-State is now', v);
+    }), {
+      '@type': 'OnOffProperty',
+      title: 'On/Off',
+      type: 'boolean',
+      description: 'Whether the lamp is turned on'
+    }));
+    this.addProperty(new Property(this, 'brightness', new Value(50, function (v) {
+      return console.log('Brightness is now', v);
+    }), {
+      '@type': 'BrightnessProperty',
+      title: 'Brightness',
+      type: 'integer',
+      description: 'The level of light from 0-100',
+      minimum: 0,
+      maximum: 100,
+      unit: 'percent'
+    }));
   }
 }
-
 /**
  * A humidity sensor which updates its measurement every few seconds.
  */
+
+
 function FakeGpioHumiditySensor() {
-  Thing.call(this,
-             'urn:dev:ops:my-humidity-sensor-1234',
-             'My Humidity Sensor',
-             ['MultiLevelSensor'],
-             'A web connected humidity sensor'
-  );
+  var _this = this;
+
+  Thing.call(this, 'urn:dev:ops:my-humidity-sensor-1234', 'My Humidity Sensor', ['MultiLevelSensor'], 'A web connected humidity sensor');
   {
     this.level = new Value(0.0);
-    this.addProperty(
-      new Property(
-        this,
-        'level',
-        this.level,
-        {
-          '@type': 'LevelProperty',
-          title: 'Humidity',
-          type: 'number',
-          description: 'The current humidity in %',
-          minimum: 0,
-          maximum: 100,
-          unit: 'percent',
-          readOnly: true,
-        }));
+    this.addProperty(new Property(this, 'level', this.level, {
+      '@type': 'LevelProperty',
+      title: 'Humidity',
+      type: 'number',
+      description: 'The current humidity in %',
+      minimum: 0,
+      maximum: 100,
+      unit: 'percent',
+      readOnly: true
+    })); // Poll the sensor reading every 3 seconds
 
-    // Poll the sensor reading every 3 seconds
-    setInterval(() => {
+    setInterval(function () {
       // Update the underlying value, which in turn notifies all listeners
-      const newLevel = this.readFromGPIO();
+      var newLevel = _this.readFromGPIO();
+
       console.log('setting new humidity level:', newLevel);
-      this.level.notifyOfExternalUpdate(newLevel);
+
+      _this.level.notifyOfExternalUpdate(newLevel);
     }, 3000);
   }
-
   /**
    * Mimic an actual sensor updating its reading every couple seconds.
    */
-  this.readFromGPIO = () => {
+
+  this.readFromGPIO = function () {
     return Math.abs(70.0 * Math.random() * (-0.5 + Math.random()));
   };
 }
 
 function runServer() {
   // Create a thing that represents a dimmable light
-  const light = new ExampleDimmableLight();
+  var light = new ExampleDimmableLight(); // Create a thing that represents a humidity sensor
 
-  // Create a thing that represents a humidity sensor
-  const sensor = new FakeGpioHumiditySensor();
-
-  // If adding more than one thing, use MultipleThings() with a name.
+  var sensor = new FakeGpioHumiditySensor(); // If adding more than one thing, use MultipleThings() with a name.
   // In the single thing case, the thing's name will be broadcast.
-  const server = new WebThingServer(new MultipleThings([light, sensor],
-                                                       'LightAndTempDevice'),
-                                    8888);
 
-  process.on('SIGINT', () => {
+  var server = new WebThingServer(new MultipleThings([light, sensor], 'LightAndTempDevice'), 8888);
+  process.on('SIGINT', function () {
     server.stop();
     process.exit();
   });
-
   server.start();
 }
 
--- example/platform/board/artik1020.js
+++ example/platform/board/artik1020.js
@@ -9,78 +9,85 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/
  */
-let webthing;
+var webthing;
+
 try {
   webthing = require('../../../webthing');
 } catch (err) {
   webthing = require('webthing-iotjs');
 }
 
-const AdcProperty = require('../adc/adc-property');
-const PwmProperty = require('../pwm/pwm-property');
+var AdcProperty = require('../adc/adc-property');
 
+var PwmProperty = require('../pwm/pwm-property');
 
 function ARTIK1020Thing(name, type, description) {
-  const self = this;
-  webthing.Thing.call(this,
-                      'urn:dev:ops:my-artik1020-1234',
-                      name || 'ARTIK1020',
-                      type || [],
-                      description || 'A web connected ARTIK1020');
-  {
-    this.pinProperties = [
-      new AdcProperty(this, 'ADC0', 0,
-                      {description: 'A0 on J24 of board'},
-                      {direction: 'in',
-                       device: '/sys/devices/12d10000.adc/iio:device0\
-/in_voltage0_raw'}),
-      new AdcProperty(this, 'ADC1', 0,
-                      {description: 'A1 on J24 of board'},
-                      {direction: 'in',
-                       device: '/sys/devices/12d10000.adc/iio:device0\
-/in_voltage1_raw'}),
-      new AdcProperty(this, 'ADC2', 0,
-                      {description: 'A2 on J24 of board'},
-                      {direction: 'in',
-                       device: '/sys/devices/12d10000.adc/iio:device0\
-/in_voltage2_raw'}),
-      new AdcProperty(this, 'ADC3', 0,
-                      {description: 'A3 on J24 of board'},
-                      {direction: 'in',
-                       device: '/sys/devices/12d10000.adc/iio:device0\
-/in_voltage5_raw'}),
-      new AdcProperty(this, 'ADC4', 0,
-                      {description: 'A4 on J24 of board'},
-                      {direction: 'in',
-                       device: '/sys/devices/12d10000.adc/iio:device0\
-/in_voltage6_raw'}),
-      new AdcProperty(this, 'ADC5', 0,
-                      {description: 'A5 on J24 of board'},
-                      {direction: 'in',
-                       device: '/sys/devices/12d10000.adc/iio:device0\
-/in_voltage7_raw'}),
-      new PwmProperty(this, 'PWM0', 50,
-                      {description: 'XPWMO1 on J26[6] of board (pwm0)'}),
+  var _this = this;
 
-      new PwmProperty(this, 'PWM1', 50,
-                      {description: 'XPWMO0 on J26[5] of board (pwm1)'},
-                      {pwm: {pin: 1}}),
-    ];
-    this.pinProperties.forEach((property) => {
+  var self = this;
+  webthing.Thing.call(this, 'urn:dev:ops:my-artik1020-1234', name || 'ARTIK1020', type || [], description || 'A web connected ARTIK1020');
+  {
+    this.pinProperties = [new AdcProperty(this, 'ADC0', 0, {
+      description: 'A0 on J24 of board'
+    }, {
+      direction: 'in',
+      device: '/sys/devices/12d10000.adc/iio:device0\
+/in_voltage0_raw'
+    }), new AdcProperty(this, 'ADC1', 0, {
+      description: 'A1 on J24 of board'
+    }, {
+      direction: 'in',
+      device: '/sys/devices/12d10000.adc/iio:device0\
+/in_voltage1_raw'
+    }), new AdcProperty(this, 'ADC2', 0, {
+      description: 'A2 on J24 of board'
+    }, {
+      direction: 'in',
+      device: '/sys/devices/12d10000.adc/iio:device0\
+/in_voltage2_raw'
+    }), new AdcProperty(this, 'ADC3', 0, {
+      description: 'A3 on J24 of board'
+    }, {
+      direction: 'in',
+      device: '/sys/devices/12d10000.adc/iio:device0\
+/in_voltage5_raw'
+    }), new AdcProperty(this, 'ADC4', 0, {
+      description: 'A4 on J24 of board'
+    }, {
+      direction: 'in',
+      device: '/sys/devices/12d10000.adc/iio:device0\
+/in_voltage6_raw'
+    }), new AdcProperty(this, 'ADC5', 0, {
+      description: 'A5 on J24 of board'
+    }, {
+      direction: 'in',
+      device: '/sys/devices/12d10000.adc/iio:device0\
+/in_voltage7_raw'
+    }), new PwmProperty(this, 'PWM0', 50, {
+      description: 'XPWMO1 on J26[6] of board (pwm0)'
+    }), new PwmProperty(this, 'PWM1', 50, {
+      description: 'XPWMO0 on J26[5] of board (pwm1)'
+    }, {
+      pwm: {
+        pin: 1
+      }
+    })];
+    this.pinProperties.forEach(function (property) {
       self.addProperty(property);
     });
   }
 
-  this.close = () => {
-    this.pinProperties.forEach((property) => {
+  this.close = function () {
+    _this.pinProperties.forEach(function (property) {
       property.close && property.close();
     });
   };
 }
 
-module.exports = function() {
+module.exports = function () {
   if (!module.exports.instance) {
     module.exports.instance = new ARTIK1020Thing();
   }
+
   return module.exports.instance;
 };
