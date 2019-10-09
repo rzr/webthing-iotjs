8a0ed973a2180c9b95965bf0207457bc80df313b
Philippe Coval <rzr@users.sf.net>
2019-10-09 10:17:33 +0200

    build: Also port travis.sh but unused yet
    Just to avoid unexpected failure
    
    Change-Id: I38039134835d06fe4bf42dc5f139ecbcde517bdd
    Conflicts: Resolved
    Last-Update: 2020-10-15
    Signed-off-by: Philippe Coval <rzr@user.sf.net>

--- test.sh
+++ test.sh
@@ -1,20 +1,9 @@
 #!/bin/bash -e
+set -x
 
-# clone the webthing-tester
-git clone https://github.com/WebThingsIO/webthing-tester
-pip3 install --user -r webthing-tester/requirements.txt
+make lint
+make build
+make check
+make test
 
-export NODE_PATH=.
-# build and test the single-thing example
-node example/single-thing.js &
-EXAMPLE_PID=$!
-sleep 5
-./webthing-tester/test-client.py --debug
-kill -15 $EXAMPLE_PID
 
-# build and test the multiple-things example
-node example/multiple-things.js &
-EXAMPLE_PID=$!
-sleep 5
-./webthing-tester/test-client.py --path-prefix "/0"
-kill -15 $EXAMPLE_PID
